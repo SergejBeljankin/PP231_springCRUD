eef4634bf0e706c30b97a5775c448342f22d5558
Belyankin Sergey <beljankin@yandex.ru>
2021-12-04 12:35:25 +0300

    CRUD концепт

--- src/main/java/ru/beljankin/spring/config/DataConfig.java
+++ src/main/java/ru/beljankin/spring/config/DataConfig.java
@@ -15,7 +15,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
